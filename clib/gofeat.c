e608a19b937c8713290bcc6b6b47e4a383feaa34
Joshua <joshua@Josh-MacBook-Pro.local>
2018-05-03 14:50:20 +0800

    passing board as string

--- clib/gofeat.c
+++ clib/gofeat.c
@@ -2,30 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void str2mtx(const char* str, int* board_mtx)
+{
+    int idx = 0;
+    int number = 0;
+    int cnt = 0;
+    for (; idx<strlen(str); idx++)
+    {
+        number = str[idx]-'0';
+        if (0<=number && number <=2)
+        {
+            board_mtx[cnt++] = number;        
+        }
+    }
+}
+
+void mtx2str(char* str, int* board_mtx)
+{
+    int idx = 0;
+    int i = 0;
+    for(; i<19*19; i++)
+    {
+        str[idx++] = '0' + board_mtx[i];
+        str[idx++] = ' ';
+    }
+    str[idx] = '\0';
+}
+
+void print_mtx(int* board_mtx)
+{
+    int i = 0, j = 0;
+    for(; i<19; i++)
+    {
+        for(j=0; j<19; j++)
+            printf("%d ", ((int**)board_mtx)[i][j]);
+        printf("\n");
+    }
+
+}
 
 static PyObject* ran(PyObject* self, PyObject* args)
 {
-    const char *mtx;
-    int sts = 100;
-    if (!PyArg_ParseTuple(args, "s", &mtx))
+    const char *str;
+    int board_mtx[19][19];
+    
+    if (!PyArg_ParseTuple(args, "s", &str))
         return NULL;
-    int idx = 0;
-    int number = 0;
-    for (; idx<strlen(mtx); idx++)
-    {
-        printf("%d ", mtx[idx]-'0');
-    }
-    printf("\n%s\n", mtx);
-    // while(mtx[idx] != '\0')
-    // {
-    //     if (mtx[idx] > '0' && mtx[idx] < '9')
-    //     {
-    //         number = mtx[idx] - '0';
-    //         printf("%d ", number);
-    //         idx += 1;
-    //     }
-    // }
-    return PyLong_FromLong(sts);
+    str2mtx(str, board_mtx);
+    char ret_str[361*2+1];
+    mtx2str(ret_str, board_mtx);
+    return Py_BuildValue("s", str);
+
 }
 
 
@@ -34,7 +61,7 @@ static PyMethodDef GofeatMethods[] = {
     {"random",  ran, METH_VARARGS,
      "Test by random function"},
 
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+    {NULL, NULL, 0, NULL}   
 };
 
 
--- clib/test.py
+++ clib/test.py
@@ -1,12 +1,20 @@
 import sys
+import cv2
 sys.path.append('./build/lib.macosx-10.7-x86_64-3.6/')
 sys.path.append('/Users/joshua/Coding/go/JoshieGo')
 from game import Board
 import gofeat
 import numpy as np
-mtx = np.asarray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-string = Board.mtx2str(mtx)
-
-print(dir(gofeat))
-gofeat.random(string)
-
+# mtx = np.ones(shape=(19, 19), dtype=np.int)
+# string = Board.mtx2str(mtx)
+# print(string)
+# print(dir(gofeat))
+# string = gofeat.random(string)
+# ret_mtx = np.fromstring(string, sep=' ', dtype=np.int).reshape(19, 19)
+import pickle
+games = pickle.load(open('go_test.pkl', 'rb'))
+for board_mtx, move in zip(games[0], games[1]):
+    board = Board(board_mtx=board_mtx)
+    canvas = board.visualize_board(grid_size=35)
+    cv2.imshow('board', canvas)
+    cv2.waitKey()
